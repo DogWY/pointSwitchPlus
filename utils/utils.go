defb4ef73dbfbfbe3526dab4c7cc8a67f675626c
jiubaor <1607282994@qq.com>
2023-10-05 09:19:47 +0800

    增添定位项目根目录函数

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 // RemoveDuplicates 删除重复元素
 func RemoveDuplicates(nums []int) []int {
 	for i := 1; i < len(nums); i++ {
@@ -19,4 +25,22 @@ func ByteToAddr(b []byte) int {
 // AddrToByte 地址转字节
 func AddrToByte(a int) []byte {
 	return []byte{byte(a >> 8), byte(a & 255)}
+}
+
+// 定位项目根目录
+func findProjectRoot(currentDir string) (string, error) {
+	for {
+		if _, err := os.Stat(filepath.Join(currentDir, "go.mod")); err == nil {
+			return currentDir, nil
+		}
+
+		newDir := filepath.Dir(currentDir)
+		if newDir == currentDir {
+			break
+		}
+
+		currentDir = newDir
+	}
+
+	return "", fmt.Errorf("项目根目录未找到")
 }
